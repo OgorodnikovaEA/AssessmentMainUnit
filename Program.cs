b061b42cc87c6979559469eab10b57db991c54ee
Ekaterina Ogorodnikova <ogorodnikova3105@mail.ru>
2024-12-13 23:50:50 +0300

    code

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int n = int.Parse(Console.ReadLine()!);
+string[] origStr = new string[n];
+
+for (int i = 0; i < origStr.Length; i++)
+{
+    Console.Write("Введите строку: ");
+    origStr[i] = (Console.ReadLine()!);
+ }
+
+int count = 0;
+
+foreach (string word in origStr)
+{
+    if (word.Length <= 3)
+    {
+        count++;
+    }
+}
+
+string[] filteredStr = new string[count];
+int j = 0;
+foreach (string word in origStr)
+{
+    if (word.Length <= 3)
+    {
+        filteredStr[j++] = word;
+    }
+}
+Console.WriteLine($"Строки, длина которых 3 и меньше символов: [{string.Join(", ", filteredStr)}]");// Выводим результат
